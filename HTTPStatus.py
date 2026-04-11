ea33ff5b2c0689a5bf330b786523b67ad1661a6d
Rishab Raj <rr0948555@gmail.com>
2026-04-11 21:21:06 +0530

    Implement user creation and retrieval endpoints

---
+++ HTTPStatus.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI,status,HTTPException
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+@app.post("/create_user",status_code=status.HTTP_201_CREATED)
+def create_user():
+    return{
+        "message":"User Created"
+    }
+
+@app.get("/user")
+def get_user():
+    return {
+        "status":"Success",
+        "message":"User Fetched",
+        "data":{
+            "name":"Alex",
+            "age":24
+        }
+    }
+
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    if user_id != 1:
+        raise HTTPException(
+            status_code=404,
+            detail="User Not Found"
+        )
+    return {
+            "id":1,
+            "name":"Alex"
+    }
+    
